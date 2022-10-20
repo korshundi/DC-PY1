486d67675fe4e88e49cb383e77fe148fe8db0ce0
korshundi <115479050+korshundi@users.noreply.github.com>
2022-10-20 03:40:23 +0300

    Add files via upload

---
+++ Лабораторная_работа_3/task8.py
@@ -0,0 +1,13 @@
+money_capital = 10000
+salary = 5000
+spend = 6000
+increase = 0.05
+
+month = 0  # количество месяцев, которое можно прожить
+exp = 1.05
+delta = salary - spend
+while money_capital + delta >= spend:
+    money_capital += delta
+    spend = spend * exp
+    month += 1
+print(month)
